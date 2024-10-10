cc70b2299a356d22f5f5b811b28335ce4b89534c
Swiftminale <minalefetene@gmail.com>
2024-10-10 14:56:07 +0300

    finished with the NavBar next add highlight

--- zembil_admin/components/layout/TopBar.tsx
+++ zembil_admin/components/layout/TopBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import Logo from "/logo.jpeg";
@@ -5,8 +6,11 @@ import { navLinks } from "@/lib/constants";
 import Link from "next/link";
 import { UserButton } from "@clerk/nextjs";
 import { Menu } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 function TopBar() {
+  const [dropdownMenu, setDropdownMenu] = React.useState(false);
+  const pathname = usePathname();
   return (
     <div className="sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 bg-blue-2 shadow-xl lg:hidden">
       <Image src={Logo} alt="logo" width={50} height={70} />
@@ -22,7 +26,23 @@ function TopBar() {
           </Link>
         ))}
       </div>
-      <div className="flex gap-4  items-center">
+      <div
+        className="relative flex gap-4  items-center"
+        onClick={() => setDropdownMenu(!dropdownMenu)}
+      >
+        {dropdownMenu && (
+          <div className="absolute top-10 right-6 flex flex-col gap-8 p-5 bg-white shadow-xl rounded-lg">
+            {navLinks.map((link) => (
+              <Link
+                href={link.url}
+                key={link.label}
+                className="flex gap-4 text-body-medium"
+              >
+                {link.icon} <p>{link.label}</p>
+              </Link>
+            ))}
+          </div>
+        )}
         <Menu className="cursor-pointer md:hidden" />
         <UserButton />
       </div>
