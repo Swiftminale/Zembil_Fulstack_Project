4a3d41182c17f023c378f899ff0cd44d19886408
Swiftminale <minalefetene@gmail.com>
2024-10-11 14:48:06 +0300

    bug found image cant be uploaded, fix bug in a few hours

--- zembil_admin/components/collections/CollectionForm.tsx
+++ zembil_admin/components/collections/CollectionForm.tsx
@@ -1,48 +1,96 @@
 "use client";
-import React from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useRouter } from "next/navigation";
+
+import { Separator } from "../ui/separator";
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-
-import { Separator } from "@/components/ui/separator";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Textarea } from "../ui/textarea";
 import ImageUpload from "../custom ui/ImageUpload";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import Delete from "../custom ui/Delete";
 
 const formSchema = z.object({
-  title: z.string().min(2).max(50),
-  discription: z.string().min(2).max(500),
+  title: z.string().min(2).max(20),
+  description: z.string().min(2).max(500).trim(),
   image: z.string(),
 });
 
-function CollectionForm() {
+interface CollectionFormProps {
+  initialData?: CollectionType | null; //Must have "?" to make it optional
+}
+
+const CollectionForm: React.FC<CollectionFormProps> = ({ initialData }) => {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      discription: "",
-      image: "",
-    },
+    defaultValues: initialData
+      ? initialData
+      : {
+          title: "",
+          description: "",
+          image: "",
+        },
   });
 
-  const onSubmit = async (value: z.infer<typeof formSchema>) => {
-    console.log(value);
+  const handleKeyPress = (
+    e:
+      | React.KeyboardEvent<HTMLInputElement>
+      | React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      const url = initialData
+        ? `/api/collections/${initialData._id}`
+        : "/api/collections";
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(values),
+      });
+      if (res.ok) {
+        setLoading(false);
+        toast.success(`Collection ${initialData ? "updated" : "created"}`);
+        window.location.href = "/collections";
+        router.push("/collections");
+      }
+    } catch (err) {
+      console.log("[collections_POST]", err);
+      toast.error("Something went wrong! Please try again.");
+    }
   };
 
   return (
     <div className="p-10">
-      <p className="text-heading2-bold">Create Collection</p>
-      <Separator className="bg-grey-1 my-4 mb-7" />
+      {initialData ? (
+        <div className="flex items-center justify-between">
+          <p className="text-heading2-bold">Edit Collection</p>
+          <Delete id={initialData._id} item="collection" />
+        </div>
+      ) : (
+        <p className="text-heading2-bold">Create Collection</p>
+      )}
+      <Separator className="bg-grey-1 mt-4 mb-7" />
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
@@ -52,7 +100,11 @@ function CollectionForm() {
               <FormItem>
                 <FormLabel>Title</FormLabel>
                 <FormControl>
-                  <Input placeholder="Title" {...field} />
+                  <Input
+                    placeholder="Title"
+                    {...field}
+                    onKeyDown={handleKeyPress}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -60,12 +112,17 @@ function CollectionForm() {
           />
           <FormField
             control={form.control}
-            name="discription"
+            name="description"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Discription</FormLabel>
+                <FormLabel>Description</FormLabel>
                 <FormControl>
-                  <Textarea placeholder="Title" {...field} rows={6} />
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    rows={5}
+                    onKeyDown={handleKeyPress}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -78,17 +135,32 @@ function CollectionForm() {
               <FormItem>
                 <FormLabel>Image</FormLabel>
                 <FormControl>
-                  <ImageUpload />
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <div className="flex gap-10">
+            <Button type="submit" className="bg-blue-1 text-white">
+              Submit
+            </Button>
+            <Button
+              type="button"
+              onClick={() => router.push("/collections")}
+              className="bg-blue-1 text-white"
+            >
+              Discard
+            </Button>
+          </div>
         </form>
       </Form>
     </div>
   );
-}
+};
 
 export default CollectionForm;
--- zembil_admin/components/custom ui/ImageUpload.tsx
+++ zembil_admin/components/custom ui/ImageUpload.tsx
@@ -1,22 +1,65 @@
-import React from "react";
-
 import { CldUploadWidget } from "next-cloudinary";
-import { Button } from "../ui/button";
-import {SquarePlus} from "lucide-react"
+import { Plus, Trash } from "lucide-react";
 
-function ImageUpload() {
-  return (
-    <CldUploadWidget uploadPreset="ryjpsjyl">
-      {({ open }) => {
-        return (
-          <Button onClick={() => open()} className="bg-grey-1 text-white">
-            <SquarePlus />
-            Upload Image
-          </Button>
-        );
-      }}
-    </CldUploadWidget>
-  );
+import { Button } from "../ui/button";
+import Image from "next/image";
+
+interface ImageUploadProps {
+  value: string[];
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
 }
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  onChange,
+  onRemove,
+  value,
+}) => {
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  return (
+    <div>
+      <div className="mb-4 flex flex-wrap items-center gap-4">
+        {value.map((url) => (
+          <div key={url} className="relative w-[200px] h-[200px]">
+            <div className="absolute top-0 right-0 z-10">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                size="sm"
+                className="bg-red-1 text-white"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
+            </div>
+            <Image
+              src={url}
+              alt="collection"
+              className="object-cover rounded-lg"
+              fill
+            />
+          </div>
+        ))}
+      </div>
+
+      <CldUploadWidget uploadPreset="myyu6boo" onUpload={onUpload}>
+        {({ open }) => {
+          return (
+            <Button
+              type="button"
+              onClick={() => open()}
+              className="bg-grey-1 text-white"
+            >
+              <Plus className="h-4 w-4 mr-2" />
+              Upload Image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+};
 
 export default ImageUpload;
