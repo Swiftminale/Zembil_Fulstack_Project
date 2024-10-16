4d2f3ef22c51cceca22c8f1965aa74031db4cdd2
Swiftminale <minalefetene@gmail.com>
2024-10-16 08:20:29 +0300

    Made some bookmark changes

--- zembil_admin/components/collections/CollectionForm.tsx
+++ zembil_admin/components/collections/CollectionForm.tsx
@@ -1,14 +1,13 @@
 "use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
+// Dependencies
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
-
-import { Separator } from "../ui/separator";
 import { z } from "zod";
-import { Button } from "@/components/ui/button";
+
+// External component imports
 import {
   Form,
   FormControl,
@@ -17,8 +16,12 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "../ui/textarea";
+import { Textarea } from "@/components/ui/textarea";
+
+// Local component imports
+import { Separator } from "../ui/separator";
 import ImageUpload from "../custom ui/ImageUpload";
 
 const formSchema = z.object({
@@ -41,6 +44,7 @@ function CollectionForm() {
   });
 
   // ---On Submit---
+
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     console.log(values);
     try {
@@ -68,7 +72,8 @@ function CollectionForm() {
       setLoading(false);
     }
   };
-
+      // ---End of On Submit---
+      
   return (
     <div className="p-10">
       <p className="text-heading2-bold">Create Collection</p>
@@ -77,6 +82,7 @@ function CollectionForm() {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           {/* Title */}
+
           <FormField
             control={form.control}
             name="title"
@@ -92,6 +98,7 @@ function CollectionForm() {
           />
 
           {/* Description */}
+
           <FormField
             control={form.control}
             name="description"
@@ -107,6 +114,7 @@ function CollectionForm() {
           />
 
           {/* Image */}
+
           <FormField
             control={form.control}
             name="image"
